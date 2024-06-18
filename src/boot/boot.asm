dad2c8bf65834668e67d915a18c8aad11c55c58b
Itamar-Dalal <dalalitamar@gmail.com>
2024-06-18 14:33:03 +0300

    hello world!

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,11 +1,23 @@
 BITS 16
 ORG 0x7c00
 
-start:
-    jmp $
+mov si, msg
+call prints
+jmp $
 
-; Padding to make the total size 512 bytes
-TIMES 510 - ($ - $$) db 0
+prints:
+	mov ah, 0x0e
+	printc:
+		mov al, [si]
+		inc si
+		cmp al, 0
+		jz return
+		int 0x10
+		jmp printc
+	return:
+		ret
 
-; Boot sector signature (magic number)
-dw 0xaa55
+msg db "Hello World!", 0
+
+TIMES 510 - ($ - $$) db 0 ; Padding to make the total size 512 bytes
+dw 0xaa55 ; Boot sector signature (magic number)
