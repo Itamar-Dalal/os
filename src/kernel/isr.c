3c59f3f44ad7a20b78765d7a326591240022c381
Itamar-Dalal <dalalitamar@gmail.com>
2024-07-05 14:12:52 +0300

    irq callback added

--- src/kernel/isr.c
+++ src/kernel/isr.c
@@ -1,8 +1,31 @@
 #include "isr.h"
 #include "screen.h"
 
+void (*isr_callbacks[16])();
+
 void isr_handler(registers_t regs) {
 	screen_print("Recieved interrupt: ");
 	screen_print_int(regs.int_no, 16);
 	screen_print("\n");
+}
+
+void irq_handler(registers_t regs)
+{
+	outb(0x20, 0x20); // Ack to the main PIC
+	if (regs.int_no >= IRQ8) { // Check if the int is from the slave PIC
+		outb(0xA0, 0x20); // Ack also to the slave PIC
+	}
+	if (isr_callbacks[regs.int_no - IRQ0] != 0) {
+		(*isr_callbacks[regs.int_no - IRQ0])();
+	}
+	else {
+		screen_print("Recieved IRQ: ");
+		screen_print_int(regs.int_no, 16);
+		screen_print("\n");
+	}
+}
+
+// Function for assigning a callback function to a specific IRQ
+void register_isr_callback(int irq, void (*callback)()) {
+	isr_callbacks[irq - IRQ0] = callback;
 }
